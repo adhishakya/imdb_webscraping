c7e877b9f7968989f20710a9252024c3f9e7836a
adhishakya <shakyaadhish@gmail.com>
2025-06-13 11:34:55 +0545

    new: files creation by scrapy

---
+++ imdb_webscraping/spiders/imdb_spider.py
@@ -0,0 +1,11 @@
+import scrapy
+
+
+class ImdbSpiderSpider(scrapy.Spider):
+    name = "imdb_spider"
+
+    def start_requests(self):
+        pass
+    
+    def parse(self, response):
+        pass
